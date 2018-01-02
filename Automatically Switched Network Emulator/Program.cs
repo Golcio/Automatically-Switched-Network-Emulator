e422da9c8c03414dda919842dc8888141225692e
Krzysztof Zając <krzysztofzajac111@gmail.com>
2018-01-02 22:44:41 +0100

    Dodaj pliki projektów.

---
+++ Automatically Switched Network Emulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatically_Switched_Network_Emulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
